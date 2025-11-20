ae0d4138decb49c6251c1cbd2b1d8bbe53d3ee99
Luciano <luciano161998@gmail.com>
2025-11-20 14:20:05 -0300

    actualizacion

--- AppNico-React/src/App.jsx
+++ AppNico-React/src/App.jsx
@@ -5,14 +5,14 @@ import Header from './components/Header'
 function App() {
   
   return (
-    <body>
+    <>
       <Header />
       <div className="tarjetas">
       <Tarjeta titulo="Deuda actual" moneda="dolar"/>
       <Tarjeta titulo="Deuda en pesos" moneda="pesos"/>
       <Tarjeta titulo="Valor USD blue" moneda="pesos"/>
       </div>
-    </body>
+    </>
   )
 }
 
--- AppNico-React/src/components/Tarjeta.jsx
+++ AppNico-React/src/components/Tarjeta.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Tarjeta({titulo,moneda, valor}) {
+export default function Tarjeta({ titulo, moneda }) {
+    const [value, setValue] = useState(0);
+
+    async function obtenerDolarBlue() {
+        try {
+            const respuesta = await fetch("https://api.bluelytics.com.ar/v2/latest");
+            if (!respuesta.ok) {
+                throw new Error("Error al obtener los datos del dólar blue");
+            }
+
+            const datos = await respuesta.json();
+            console.log("Dólar blue:", datos.blue.value_sell);
+            let valorBlue = datos.blue.value_sell;
+
+        } catch (error) {
+            console.error("Hubo un problema:", error.message);
+            alert(error);
+        }
+    }
+
+
     return (
         <div className="tarjeta">
             <div className="tarjeta-header">{titulo}</div>
-            <div className="tarjeta-content"><p>{moneda == "pesos" ? "$" : "USD$"}</p>{valor}</div>
-        </div>  
+            <div className="tarjeta-content"><p>{moneda == "pesos" ? "$" : "USD$"}</p>{valorBlue}</div>
+        </div>
     )
 }
